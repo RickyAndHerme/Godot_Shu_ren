4c92f1c4da3c4a8c332f522312c65a8757de39de
リッキー <98391779+RickyAndHerme@users.noreply.github.com>
2022-02-02 18:14:22 +0900

    Delete SampleNovelGame_v101 directory

--- SampleNovelGame_v101/2gyunyu.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func _process(delta):
-	if Global.lines == 15 and Input.is_action_just_pressed("click"):	#現在行が13であり、左クリックが入力されたら
-		$TextWindow.visible = false
-		Global.scene_move("res://3dekake.tscn")
