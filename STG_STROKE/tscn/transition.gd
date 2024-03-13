126195639e7459077485f10b344c410984fa4e2c
リッキー <98391779+RickyAndHerme@users.noreply.github.com>
2024-03-13 13:09:23 +0900

    Update transition.gd

--- STG_STROKE/tscn/transition.gd
+++ STG_STROKE/tscn/transition.gd
@@ -4,12 +4,12 @@ extends CanvasLayer
 #フェードアウト/インでシーンを切り替える
 #===============================================
 func fade(path):
-	%AnimationPlayer.play("fade")
+	%AnimationPlayer.play(&"fade")
 	await get_tree().create_timer(1.0).timeout		#1秒停止している
 	get_tree().change_scene_to_file(path)
-	%AnimationPlayer.play_backwards("fade")
+	%AnimationPlayer.play_backwards(&"fade")
 	await get_tree().create_timer(1.0).timeout
-	%AnimationPlayer.play("RESET")
+	%AnimationPlayer.play(&"RESET")
 
 
 
